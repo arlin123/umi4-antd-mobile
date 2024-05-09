b71bf869ba28660d28adb62fe3cbf7b56904dc4a
penglin <penglin9999@gmail.com>
2024-05-09 14:45:15 +0800

    fix: 清理

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,52 @@
-import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
-import { PageContainer } from '@ant-design/pro-components';
-import { useModel } from '@umijs/max';
-import styles from './index.less';
+import { Button, ResultPage, Space } from 'antd-mobile';
+import { AlipayCircleFill } from 'antd-mobile-icons';
 
-const HomePage: React.FC = () => {
-  const { name } = useModel('global');
+export default () => {
+  const Card = ResultPage.Card;
+
+  const details = [
+    {
+      label: '肯德基（嘉里中心店）',
+      value: '¥ 36.50',
+      bold: true,
+    },
+    {
+      label: '付款方式',
+      value: '账户余额',
+    },
+  ];
+
   return (
-    <PageContainer ghost>
-      <div className={styles.container}>
-        <Guide name={trim(name)} />
-      </div>
-    </PageContainer>
+    <>
+      <ResultPage
+        status="waiting"
+        title={<div style={{ fontSize: 15 }}>支付成功</div>}
+        description={
+          <>
+            <span style={{ fontSize: 32, color: '#ffffff', marginRight: 4 }}>
+              ¥
+            </span>
+            <span style={{ fontSize: 48, color: '#ffffff' }}>36.50</span>
+          </>
+        }
+        icon={<AlipayCircleFill />}
+        details={details}
+        // secondaryButtonText="辅助操作"
+        // primaryButtonText="主要操作"
+      >
+        <Card style={{ height: 100 }}>
+          <Space>
+            <Button>按钮1</Button>
+            <Button>按钮2</Button>
+            <Button>按钮3</Button>
+          </Space>
+        </Card>
+        <Card style={{ height: 128, marginTop: 12 }}> </Card>
+        <Card style={{ height: 128, marginTop: 12 }}> </Card>
+        <Card style={{ height: 128, marginTop: 12 }}> </Card>
+        <Card style={{ height: 128, marginTop: 12 }}> </Card>
+        <Card style={{ height: 128, marginTop: 12 }}> </Card>
+      </ResultPage>
+    </>
   );
 };
-
-export default HomePage;
